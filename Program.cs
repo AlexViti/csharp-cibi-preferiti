876eab068fe748ab756d20a901991dacdd8b06fd
AlexViti <96179738+AlexViti@users.noreply.github.com>
2022-06-23 14:40:26 +0200

    Add bonus

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] favoriteFoods = { "pizza", "burgers", "sushi", "steak", "apples", "carrots" };
+
+Console.WriteLine("Array length: " + favoriteFoods.Length);
+
+foreach (string food in favoriteFoods)
+{
+    Console.WriteLine(food);
+}
+
+Console.WriteLine("My favorite food: " + favoriteFoods[0]);
+
+Console.WriteLine("My favorite food: " + favoriteFoods[favoriteFoods.Length - 1]);
+
+// Bonus
+
+if (favoriteFoods.Length % 2 == 0)
+{
+    Console.WriteLine("The middle elements are: " + favoriteFoods[favoriteFoods.Length / 2] + " and " + favoriteFoods[favoriteFoods.Length / 2 + 1]);
+}
+else
+{
+    Console.WriteLine("The middle element is: " + favoriteFoods[favoriteFoods.Length / 2]);
+}

a3e14c9efc555a2a87b412f2586f9731edbaf134
AlexViti <96179738+AlexViti@users.noreply.github.com>
2022-06-23 14:57:35 +0200

    Minor tweaks

--- Program.cs
+++ Program.cs
@@ -1,25 +1,25 @@
 ﻿string[] favoriteFoods = { "pizza", "burgers", "sushi", "steak", "apples" };
 
-Console.WriteLine("Array length: " + favoriteFoods.Length);
+Console.WriteLine($"Array length: {favoriteFoods.Length}");
 
 Console.WriteLine("My favorite foods are: ");
 
 for (int i = 0; i < favoriteFoods.Length; i++)
 {
-    Console.WriteLine(" " + i + 1 + ". " + favoriteFoods[i]);
+    Console.WriteLine($"  {i}. {favoriteFoods[i]}");
 }
 
-Console.WriteLine("My favorite food of all: " + favoriteFoods[0]);
+Console.WriteLine($"My favorite food of all is: {favoriteFoods[0]}");
 
-Console.WriteLine("My least favorite food: " + favoriteFoods[favoriteFoods.Length - 1]);
+Console.WriteLine($"My least favorite food is: {favoriteFoods[favoriteFoods.Length - 1]}");
 
 // Bonus
 
 if (favoriteFoods.Length % 2 == 0)
 {
-    Console.WriteLine("The middle elements are: " + favoriteFoods[favoriteFoods.Length / 2 - 1] + " and " + favoriteFoods[favoriteFoods.Length / 2]);
+    Console.WriteLine($"The middle elements are: {favoriteFoods[favoriteFoods.Length / 2 - 1]} and {favoriteFoods[favoriteFoods.Length / 2]}");
 }
 else
 {
-    Console.WriteLine("The middle element is: " + favoriteFoods[favoriteFoods.Length / 2]);
+    Console.WriteLine($"The middle element is: {favoriteFoods[favoriteFoods.Length / 2]}");
 }

bc6e457e2de6f296562ccc32a68de45e43e1b21b
AlexViti <96179738+AlexViti@users.noreply.github.com>
2022-06-23 15:10:22 +0200

    Add randomization of list length

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿string[] favoriteFoods = { "pizza", "burgers", "sushi", "steak", "apples" };
+﻿string[] favoriteFoodsOdd = { "pizza", "burgers", "sushi", "steak", "apples" };
+string[] favoriteFoodsEven = favoriteFoodsOdd.Concat(new string[] { "carrots" }).ToArray();
+
+Random random = new Random();
+int randomNumber = random.Next(0, 2);
+
+string[] favoriteFoods = randomNumber == 0 ? favoriteFoodsEven : favoriteFoodsOdd;
 
 Console.WriteLine($"Array length: {favoriteFoods.Length}");
 
@@ -6,7 +12,7 @@ Console.WriteLine("My favorite foods are: ");
 
 for (int i = 0; i < favoriteFoods.Length; i++)
 {
-    Console.WriteLine($"  {i}. {favoriteFoods[i]}");
+    Console.WriteLine($"  {i + 1}. {favoriteFoods[i]}");
 }
 
 Console.WriteLine($"My favorite food of all is: {favoriteFoods[0]}");

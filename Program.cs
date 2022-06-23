16d52479b05e23c9cc71630486b68d81192c81f9
AlexViti <96179738+AlexViti@users.noreply.github.com>
2022-06-23 14:50:25 +0200

    fix middle elements

--- Program.cs
+++ Program.cs
@@ -1,21 +1,23 @@
-﻿string[] favoriteFoods = { "pizza", "burgers", "sushi", "steak", "apples", "carrots" };
+﻿string[] favoriteFoods = { "pizza", "burgers", "sushi", "steak", "apples" };
 
 Console.WriteLine("Array length: " + favoriteFoods.Length);
 
-foreach (string food in favoriteFoods)
+Console.WriteLine("My favorite foods are: ");
+
+for (int i = 0; i < favoriteFoods.Length; i++)
 {
-    Console.WriteLine(food);
+    Console.WriteLine(" " + i + 1 + ". " + favoriteFoods[i]);
 }
 
-Console.WriteLine("My favorite food: " + favoriteFoods[0]);
+Console.WriteLine("My favorite food of all: " + favoriteFoods[0]);
 
-Console.WriteLine("My favorite food: " + favoriteFoods[favoriteFoods.Length - 1]);
+Console.WriteLine("My least favorite food: " + favoriteFoods[favoriteFoods.Length - 1]);
 
 // Bonus
 
 if (favoriteFoods.Length % 2 == 0)
 {
-    Console.WriteLine("The middle elements are: " + favoriteFoods[favoriteFoods.Length / 2] + " and " + favoriteFoods[favoriteFoods.Length / 2 + 1]);
+    Console.WriteLine("The middle elements are: " + favoriteFoods[favoriteFoods.Length / 2 - 1] + " and " + favoriteFoods[favoriteFoods.Length / 2]);
 }
 else
 {
